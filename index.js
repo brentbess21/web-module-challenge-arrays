151662ebc8f3a1cfa2ffdcd3b919753aaebf81d1
Brent Besselievre <brentbess21@gmail.com>
2021-07-14 11:53:25 -0600

    Solved Task 1

--- index.js
+++ index.js
@@ -45,10 +45,11 @@ Use the copy function below to do the following:
   2. Return a copy of the received array  
 */
 
-function copy(){
-  
+function copy(array){
+  return [...array];
 }    
 
+console.log("Task 1 - " + copy(originalFlavors));
 
 
 
